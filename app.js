fa187b8e5f8c715283523befeebda4fb0b54deef
Artur Guedes <arturguedx@gmail.com>
2019-10-27 09:54:46 -0300

    .

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ app.onStart(() => {
     };
 });
 
-app.builtInIntent('unihack', 'Fala oi pro pessoal', () => {return{text:'Oi pessoal, desculpe minha falta de educação. Não sabia que tinha mais gente aqui',end:false}});
+app.builtInIntent('unihack', 'Fala oi pro pessoal', () => {
+    return{
+        text:'Oi pessoal, desculpe minha falta de educação. Não sabia que tinha mais gente aqui',
+        end:false
+    }
+});
 
 app.intent('HelloIntent', 'Hello', () => {
     return {text:'Entendi nada ra ra ra',end:false};
